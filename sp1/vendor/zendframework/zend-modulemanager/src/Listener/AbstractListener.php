3c9ce2610ad49cc1e7caf49021d492554a2dc327
akshay <akshay.vyas@people-tree.com>
2016-11-14 16:01:16 +0530

    comment code
    remove \n

--- sp1/vendor/zendframework/zend-modulemanager/src/Listener/AbstractListener.php
+++ sp1/vendor/zendframework/zend-modulemanager/src/Listener/AbstractListener.php
@@ -61,7 +61,7 @@ abstract class AbstractListener
      */
     protected function writeArrayToFile($filePath, $array)
     {
-        $content = "<?php\nreturn " . var_export($array, 1) . ';';
+        $content = "<?php return " . var_export($array, 1) . ';';
         file_put_contents($filePath, $content);
         return $this;
     }
